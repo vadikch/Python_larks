a90e991930a4c20e1d8f632c87c2c4f94dc0aeaa
vadikch <v.v.tarasevich@gmail.com>
2023-03-09 11:36:42 +0100

    my second commit

--- Hello_World.py
+++ Hello_World.py
@@ -5,4 +5,5 @@ print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
 print('What is your age?')    # ask for their age
-myAge = input()print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
